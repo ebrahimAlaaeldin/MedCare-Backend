bd606fb1bd542e3fdbde5b2816545e2876c4e73c
ebrahimAlaaeldin <ebrahim.alaa2627@yahoo.com>
2024-12-01 17:13:52 +0200

    first commit

---
+++ src/main/java/com/example/medcare/MedCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.medcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedCareApplication.class, args);
+    }
+
+}
