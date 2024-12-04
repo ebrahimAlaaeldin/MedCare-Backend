458877a82b6d679ebcf035c878e97c8e63f0c81c
abdalllah911 <abdallahadel667@gmail.com>
2024-12-04 07:49:07 +0200

    jakarta mail Dependencies added

--- src/main/java/com/example/medcare/service/EmailService.java
+++ src/main/java/com/example/medcare/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.example.medcare.service;
+
 import com.example.medcare.dto.MailBody;
+
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
@@ -7,6 +9,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @AllArgsConstructor
