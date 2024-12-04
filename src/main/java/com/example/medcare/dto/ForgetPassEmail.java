62a9cf365621bc1333dd3e82816a38fce6b30061
Youssef alaa <youssef.alaa.ahmed.haredy@gmail.com>
2024-12-04 06:26:38 +0200

    Tests for ForgetResetPasswordService

--- src/main/java/com/example/medcare/dto/ForgetPassEmail.java
+++ src/main/java/com/example/medcare/dto/ForgetPassEmail.java
@@ -1,8 +1,10 @@
 package com.example.medcare.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ForgetPassEmail {
 
     private String email;
--- src/main/java/com/example/medcare/dto/ResetPasswordDto.java
+++ src/main/java/com/example/medcare/dto/ResetPasswordDto.java
@@ -1,8 +1,10 @@
 package com.example.medcare.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ResetPasswordDto {
     private String oldPassword;
     private String newPassword;
