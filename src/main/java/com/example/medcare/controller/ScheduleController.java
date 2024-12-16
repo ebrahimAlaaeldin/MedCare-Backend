3f936e74dd8841aef7fe38b34526481914f7672d
Amir-Ragaie <amir.ragaie.soliman@gmail.com>
2024-12-16 21:38:27 +0200

    change return types to handle different HTTP responses

--- src/main/java/com/example/medcare/controller/ScheduleController.java
+++ src/main/java/com/example/medcare/controller/ScheduleController.java
@@ -7,6 +7,8 @@ import com.example.medcare.entities.Appointment;
 import com.example.medcare.service.ScheduleAppointmentService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +24,17 @@ public class ScheduleController {
     private final ScheduleAppointmentService scheduleAppointmentService;
 
     @PostMapping("/schedule")
-    public ResponseEntity<ResponseMessageDto> scheduleAppointment(
+    public ResponseEntity<Object> scheduleAppointment(
             @RequestBody AppointmentDTO requestForAppointment) {
 
-        // Schedule an appointment
-        return ResponseEntity.ok(scheduleAppointmentService.scheduleAppointment(requestForAppointment));
-    }
+        
+        if(requestForAppointment.getPatientId() == null || requestForAppointment.getDoctorId() == null || requestForAppointment.getAppointmentTime() == null) {
+            return ResponseEntity.badRequest().body(ResponseMessageDto.builder()
+                    .message("Please provide all the required fields")
+                    .success(false)
+                    .statusCode(400)
+                    .build());
+        }
+
+        return scheduleAppointmentService.scheduleAppointment(requestForAppointment);       }
 }
--- src/main/java/com/example/medcare/service/ScheduleAppointmentService.java
+++ src/main/java/com/example/medcare/service/ScheduleAppointmentService.java
@@ -7,6 +7,8 @@ import com.example.medcare.repository.AppointmentRepository;
 import com.example.medcare.repository.PatientRepository;
 import com.example.medcare.repository.DoctorRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,7 +19,7 @@ public class ScheduleAppointmentService {
     private final PatientRepository patientRepository;
     private final DoctorRepository doctorRepository;
 
-    public ResponseMessageDto scheduleAppointment(AppointmentDTO appointmentDTO){
+    public ResponseEntity<Object> scheduleAppointment(AppointmentDTO appointmentDTO){
 
         Appointment appointment = new Appointment();
 
@@ -26,11 +28,11 @@ public class ScheduleAppointmentService {
 
         if(patient.isEmpty()){
 
-            return ResponseMessageDto.builder()
+            return ResponseEntity.status(404).body(ResponseMessageDto.builder()
             .message("Patient Not Found")
             .success(false)
-            .statusCode(401)
-            .build();
+            .statusCode(404)
+            .build());
         }
         
 
@@ -39,23 +41,23 @@ public class ScheduleAppointmentService {
 
         if(doctor.isEmpty()){
 
-            return ResponseMessageDto.builder()
+            return ResponseEntity.status(404).body(ResponseMessageDto.builder()
             .message("Doctor Not Found")
             .success(false)
-            .statusCode(401)
-            .build();
+            .statusCode(404)
+            .build());
         }
-
+        
         //check if appointment time is available
         boolean existsAnAppointment = appointmentRepository.existsByDoctorIdAndAppointmentTime(appointmentDTO.getDoctorId(), appointmentDTO.getAppointmentTime());
         
         if(existsAnAppointment){
 
-            return ResponseMessageDto.builder()
-            .message("Appointment Time Not Available")
+            return ResponseEntity.status(409).body(ResponseMessageDto.builder()
+            .message("This time slot is reserved.")
             .success(false)
-            .statusCode(401)
-            .build();
+            .statusCode(409)
+            .build());
         }
 
         //save appointment
@@ -68,10 +70,10 @@ public class ScheduleAppointmentService {
         //save to database
         appointmentRepository.save(appointment);
 
-        return ResponseMessageDto.builder()
+        return ResponseEntity.ok().body(ResponseMessageDto.builder()
         .message("Appointment Scheduled Successfully")
         .success(true)
         .statusCode(200)
-        .build();
+        .build());
     }
 }
