923b3d1c259d4361a81f060d4fbb889758858c89
Youssef alaa <youssef.alaa.ahmed.haredy@gmail.com>
2025-01-01 06:50:44 +0200

    some changes

--- src/main/java/com/example/medcare/repository/DoctorRepository.java
+++ src/main/java/com/example/medcare/repository/DoctorRepository.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,15 +15,18 @@ public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 
     List<Doctor> findAllByIsVerified(Boolean isVerified);
 
-//     Optional<Doctor> findByUsername(String username);
+    Optional<Doctor> findByUsername(String username);
 
-    List<Doctor> findBySpecializationAndClinicId(String specialization, int clinicId);
-
-
-
-
-    @Query("SELECT d FROM Doctor d join User u  on u.id = d.id WHERE u.username = :username")
-    Optional<Doctor> findByUsername(@Param("username") String username);
-
+    @Query("""
+    SELECT d
+    FROM Doctor d
+    JOIN d.clinics c
+    WHERE d.license.specialty = :specialization
+    AND c.clinicId = :clinicId
+""")
+    List<Doctor> findDoctorsBySpecializationAndClinicId(
+            @Param("specialization") String specialization,
+            @Param("clinicId") int clinicId
+    );
 
 }
